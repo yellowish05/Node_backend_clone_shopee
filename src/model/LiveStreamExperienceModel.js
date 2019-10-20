d3712a4e4944c4866e060b71a63c19b0e236a86d
Victor Ostapchuk <victor.ostapchuk@gmail.com>
2019-10-21 02:34:03 +0300

    MVP-209 move experience data to model

---
+++ src/model/LiveStreamExperienceModel.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    id: '81a42796-8481-46ef-9eac-ed49927fc21d', order: 1, name: 'Events', description: 'Enjoy the live streaming experience at an event. Whether it is a concert, convention, special appearance, performance, tournament, expo, gala, or festival.', image: null,
+  },
+  {
+    id: '100103a9-93a6-4114-bb9c-fe348ee4ca7c', order: 2, name: 'Influencers', description: 'Connecting you to your favorite brand ambassadors, social media influencers, and celebrities! Watch them showcase products in their glamorous fashion.', image: null,
+  },
+  {
+    id: '1e086e85-c6b2-4738-942c-b8ad51735555', order: 3, name: 'Personal Shoppers', description: 'Follow an independent personal shopper while he or she purchases your orders and gives you advice and suggestions on the latest tips and styles.', image: null,
+  },
+  {
+    id: 'ff27a82a-31b7-4bb8-904a-8ff8875d6c7d', order: 4, name: 'Brand Advocate', description: 'Discover brand advocates who love their experiences with a certain brand/product and can\'t wait to share it with you.', image: null,
+  },
+  {
+    id: '9330d1e1-48e1-4cf9-a12c-4fb716ccc8d4', order: 5, name: 'Luxury Goods', description: 'Watch live at your favorite boutique destinations as you get presented with stylish clothing, jewelry, or other luxury goods.', image: null,
+  },
+  {
+    id: 'a025aa70-38e4-4ff6-b44b-64cfe1f9e189', order: 6, name: 'Crafted & Personalized', description: 'Looking for custom, handmade, vintage, or unique products? Browse livestreams by independent sellers and artisans for your crafted and personalized products.', image: null,
+  },
+  {
+    id: '704f3d9b-38be-43e7-badc-8b0c0500413d', order: 7, name: 'Local Classified', description: 'Find your local classified livestream for cars, real estate, and everything else.', image: null,
+  },
+  {
+    id: 'd3544591-dab4-4457-86f1-82baadc6b7d2', order: 8, name: 'Tips & Life Hacks', description: 'Look for amazing tips and life hacks for doing everything better than before.', image: null,
+  },
+  {
+    id: '68252d52-3195-43c4-8c83-39f3bbbdb1c5', order: 9, name: 'Seasonal Sales', description: 'It\'s a bargain shopper\'s dream come true. Take advantage of this end-of-the-season sale and watch your favorite products go on sale this season live.', image: null,
+  },
+  {
+    id: '8a261ffd-8daa-4dbb-aca4-5978b638f107', order: 10, name: 'Wholesalers', description: 'Get the lowest prices possiible in purchasing bulk products directly from the factories, manuafactureres, suppliers, importers, and wholesalers.', image: null,
+  },
+];
