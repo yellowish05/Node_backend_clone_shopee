eb1e46227c951be2947d986563877ed41aa2067f
Nykyta Yevdokymenko <redlinepech@gmail.com>
2019-10-24 21:54:29 +0300

    MVP-279 remove mongo id errors

--- src/model/commonFields/UUIDField.js
+++ src/model/commonFields/UUIDField.js
@@ -4,7 +4,6 @@ const uuidField = {
     type: String,
     lowercase: true,
     match: [/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i, 'ID is not correct!'],
-    index: { unique: true },
   },
 };
 
