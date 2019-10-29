9352d714782aef454de1937d27ffd20c396612c3
jarikexe <jarikexe>
2019-10-29 19:13:22 +0200

    MVP-304 add images to categories

--- src/model/LiveStreamCategoryModel.js
+++ src/model/LiveStreamCategoryModel.js
@@ -1,44 +1,46 @@
+const { cdn } = require('../../config');
+
 module.exports = [
   {
-    id: 'd713ee93-60b7-479f-ae39-b4e554495a29', order: 1, name: 'Women Fashion', image: null,
+    id: 'd713ee93-60b7-479f-ae39-b4e554495a29', order: 1, name: 'Women Fashion', image:  cdn.url + "/categories/women-fashion.jpg",
   },
   {
-    id: '48b1990c-90e3-494c-89f3-e0a2edc92522', order: 2, name: 'Men Fashion', image: null,
+    id: '48b1990c-90e3-494c-89f3-e0a2edc92522', order: 2, name: 'Men Fashion', image:  cdn.url + "/categories/men-fashion.jpg",
   },
   {
-    id: '0e7e17af-d955-49df-b179-e2eafe552054', order: 3, name: 'Kids Fashion', image: null,
+    id: '0e7e17af-d955-49df-b179-e2eafe552054', order: 3, name: 'Kids Fashion', image:  cdn.url + "/categories/kids-fashion.jpg",
   },
   {
-    id: '0afb0136-70b2-48fd-9d6e-0157ba5788ff', order: 4, name: 'Footwear', image: null,
+    id: '0afb0136-70b2-48fd-9d6e-0157ba5788ff', order: 4, name: 'Footwear', image:  cdn.url + "/categories/footwear.jpg",
   },
   {
-    id: '906d886f-6183-4071-809c-f56585367404', order: 5, name: 'Consumer Electronics', image: null,
+    id: '906d886f-6183-4071-809c-f56585367404', order: 5, name: 'Consumer Electronics', image:  cdn.url + "/categories/consumer-electronics.jpg",
   },
   {
-    id: '4a72e210-599b-449d-856b-1c471a507869', order: 6, name: 'Health & Beauty', image: null,
+    id: '4a72e210-599b-449d-856b-1c471a507869', order: 6, name: 'Health & Beauty', image:  cdn.url + "/categories/health-and-beauty.jpg",
   },
   {
-    id: 'b22346b6-87ac-4c3b-84af-8585bd519985', order: 7, name: 'Home Goods', image: null,
+    id: 'b22346b6-87ac-4c3b-84af-8585bd519985', order: 7, name: 'Home Goods', image:  cdn.url + "/categories/homegoods.jpg",
   },
   {
-    id: '77be0706-1c4f-45d1-a8cb-0faf8c7f5280', order: 8, name: 'Media & Entertainment', image: null,
+    id: '77be0706-1c4f-45d1-a8cb-0faf8c7f5280', order: 8, name: 'Media & Entertainment', image:  cdn.url + "/categories/media-and-entertainment.jpg",
   },
   {
-    id: 'c9d21db5-0738-4efa-ade6-a120ff21af1b', order: 9, name: 'Tickets & Reservations', image: null,
+    id: 'c9d21db5-0738-4efa-ade6-a120ff21af1b', order: 9, name: 'Tickets & Reservations', image:  cdn.url + "/categories/tickets-and-reservation.jpg",
   },
   {
-    id: '72c8ad36-6204-4f11-b51b-487c93e1530f', order: 10, name: 'Sporting Goods', image: null,
+    id: '72c8ad36-6204-4f11-b51b-487c93e1530f', order: 10, name: 'Sporting Goods', image:  cdn.url + "/categories/sport-goods.jpg",
   },
   {
-    id: '630e6ad9-942f-4911-9629-0d6cca87513a', order: 11, name: 'Automotive', image: null,
+    id: '630e6ad9-942f-4911-9629-0d6cca87513a', order: 11, name: 'Automotive', image:  cdn.url + "/categories/automobiles.jpg",
   },
   {
-    id: 'e54e53cc-6568-46b4-a481-98e98cf60b3b', order: 12, name: 'Real Estate', image: null,
+    id: 'e54e53cc-6568-46b4-a481-98e98cf60b3b', order: 12, name: 'Real Estate', image:  cdn.url + "/categories/real-estate.jpg",
   },
   {
-    id: 'e516cb92-7a9b-4618-93ef-ad8b535c7b60', order: 13, name: 'Toys & Collectibles', image: null,
+    id: 'e516cb92-7a9b-4618-93ef-ad8b535c7b60', order: 13, name: 'Toys & Collectibles', image:  cdn.url + "/categories/toys-and-collectibles.jpg",
   },
   {
-    id: '1b4c86c2-0896-4513-90dc-c944913bf016', order: 14, name: 'Adult-Only', image: null,
+    id: '1b4c86c2-0896-4513-90dc-c944913bf016', order: 14, name: 'Adult-Only', image:  cdn.url + "/categories/adult-only.jpg",
   },
 ];
