66972904ac45d92e90b1bdf25de8675da58801b7
Ubuntu <jws19950122@outlook.com>
2020-06-11 19:35:42 +0000

    update

--- src/graphql/schema/liveStream/resolvers/likeLiveStream.js
+++ src/graphql/schema/liveStream/resolvers/likeLiveStream.js
@@ -28,33 +28,3 @@ module.exports = async (obj, args, { dataSources: { repository }, user }) => {
       return liveStream;
     });
 };
-=======
-const path = require('path');
-const { Validator } = require('node-input-validator');
-
-const { ErrorHandler } = require(path.resolve('src/lib/ErrorHandler'));
-const pubsub = require(path.resolve('config/pubsub'));
-
-const errorHandler = new ErrorHandler();
-
-module.exports = async (obj, args, { dataSources: { repository }, user }) => {
-  const validator = new Validator(args, {
-    id: 'required',
-  });
-
-  return validator.check()
-    .then(async (matched) => {
-      if (!matched) {
-        throw errorHandler.build(validator.errors);
-      }
-    })
-    .then(() => repository.like.toggleLike(`LiveStream:${args.id}`, user.id))
-    .then(() => {
-      if (repository.like.load(`LiveStream:${args.id}`, user.id)) { repository.liveStream.toggleLike(args.id, 1); } else { repository.liveStream.toggleLike(args.id, -1); }
-    })
-    .then(() => repository.liveStream.load(args.id))
-    .then((liveStream) => {
-      pubsub.publish('LIVE_STREAM_CHANGE', { id: liveStream._id, ...liveStream.toObject() });
-      return liveStream;
-    });
-};
