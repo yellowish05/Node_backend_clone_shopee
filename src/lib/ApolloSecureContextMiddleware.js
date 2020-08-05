3ad0eeee6abdc0cc52159757f4d0a17956b24086
Superstar <freewebdev0213@gmail.com>
2020-08-05 20:16:39 +0800

    Update Authentication API (0805)

--- src/lib/ApolloSecureContextMiddleware.js
+++ src/lib/ApolloSecureContextMiddleware.js
@@ -4,6 +4,8 @@ const JWT_REGEX = /^[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*$/;
 
 const path = require('path');
 const serviceAccount = require(path.resolve('config/adminSDK.json'));
+const axios = require('axios');
+const logger = require('../../config/logger');
 
 function fetchAuthorization({ req, connection }) {
   if (connection) {
@@ -49,12 +51,19 @@ module.exports = (repository) => async (request) => {
 
   const accessToken = await repository.accessToken.load(data.uid);
 
-  try {
-    jwt.verify(token, serviceAccount.private_key, { algorithms: "RS256" });
-    // jwt.verify(token, accessToken._id, { algorithms: "RS256" });
-  } catch (error) {
-    throw new AuthenticationError('Invalid token');
-  }
+  axios.get(serviceAccount.client_x509_cert_url)
+  .then((response) => {
+    const public_key = response.data[serviceAccount.private_key_id];
+    try {
+      jwt.verify(token, public_key, { algorithms: "RS256" });
+    } catch (error) {
+      throw new AuthenticationError('Invalid token');
+    }
+  })
+  .catch((err) => {
+    logger.error(err);
+    throw new AuthenticationError('User Authentication Failed');
+  });
 
   const user = await repository.user.load(accessToken.user);
   return { user };
