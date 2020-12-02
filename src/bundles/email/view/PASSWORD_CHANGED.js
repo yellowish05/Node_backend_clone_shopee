6c18f40280126b68f3f1e94731e4065301f2d3cd
Austin <aripang.com@gmail.com>
2020-12-02 17:48:22 +0800

    email bundle templates;

--- src/bundles/email/view/PASSWORD_CHANGED.js
+++ src/bundles/email/view/PASSWORD_CHANGED.js
@@ -13,23 +13,29 @@ module.exports = {
         header({title: 'Update!', description: 'Your password has been changed.'}) + `
         <tr class="body password-changed">
             <td>
-                <p class="title-color">
-                    Hi${args.user.name ?  ' ' + args.user.name: ''},
-                </p>
-                <p>
-                    The password for the Shoclef account <a class="link link-default" href="#">${args.user.email}</a> was just changed.
-                </p>
-                <p>
-                    If this was you, then you can safely ignore this email.
-                </p>
-                <p>
-                    If this wasn’t you, 
-                    <a class="link" href="mailto:${email.supportEmail}">contact our support</a>.
-                </p>
-                <p>
-                    Cheers, <br>  
-                    Shoclef Corporation!
-                </p>
+                <table class="table1" width="600" align="center" border="0" cellspacing="0" cellpadding="0">
+                    <tr>
+                        <td bgcolor="#fcfcfc" style="padding: 20px 20px;border: 1px solid #f2f2f2;border-radius: 5px;">
+                            <p class="title-color">
+                                Hi${args.user.name ?  ' ' + args.user.name: ''},
+                            </p>
+                            <p>
+                                The password for the Shoclef account <a class="link link-default" href="#">${args.user.email}</a> was just changed.
+                            </p>
+                            <p>
+                                If this was you, then you can safely ignore this email.
+                            </p>
+                            <p>
+                                If this wasn’t you, 
+                                <a class="link" href="mailto:${email.supportEmail}">contact our support</a>.
+                            </p>
+                            <p>
+                                Cheers, <br>  
+                                Shoclef Corporation!
+                            </p>
+                        </td>
+                    </tr>
+                </table>
             </td>
         </tr>
     ` + footer(), args);
--- src/bundles/email/view/RESET_PASSWORD.js
+++ src/bundles/email/view/RESET_PASSWORD.js
@@ -13,31 +13,34 @@ module.exports = {
       `${header({ title: 'Notice!', description: 'You told us you forgot your password.' })}
         <tr class="body reset-password">
             <td>
-                <p class="title-color">
-                    Hi${args.user.name ? ` ${args.user.name}` : ''},
-                </p>
-                <p>
-                    We received a request to reset your password for your Shoclef account: 
-                    <a class="link link-default" href="#">${args.user.email}</a>. We’re here to help!
-                </p>
-                <p>
-                    Simply use this code to set a new password:
-                </p>
-                <p class="reset-action">
-                    <b>${code}</b>
-                </p>
-                <p>
-                    If you didn’t ask to change your password, 
-                    <a class="link" href="mailto:${email.supportEmail}">contact our support</a>! 
-                    Don’t worry, your password is still safe and you can delete this email.
-                </p>
-                <p>
-                    Cheers, <br>  
-                    Shoclef Corporation!
-                </p>
-                <p class="action">
-                    If you’re having trouble with the button above, copy and paste the URL below into your web browser. ${code}
-                </p>
+                <table class="table1" width="600" align="center" border="0" cellspacing="0" cellpadding="0">
+                    <tr>
+                        <td bgcolor="#fcfcfc" style="padding: 20px 20px;border: 1px solid #f2f2f2;border-radius: 5px;">
+                            <p class="title-color">
+                                Hi${args.user.name ? ` ${args.user.name}` : ''},
+                            </p>
+                            <p>
+                                We received a request to reset your password for your Shoclef account: 
+                                <a class="link link-default" href="#">${args.user.email}</a>. We’re here to help!
+                            </p>
+                            <p>
+                                Simply use this code to set a new password:
+                            </p>
+                            <p class="reset-action">
+                                <b>${code}</b>
+                            </p>
+                            <p>
+                                If you didn’t ask to change your password, 
+                                <a class="link" href="mailto:${email.supportEmail}">contact our support</a>! 
+                                Don’t worry, your password is still safe and you can delete this email.
+                            </p>
+                            <p>
+                                Cheers, <br>  
+                                Shoclef Corporation!
+                            </p>
+                        </td>
+                    </tr>
+                </table>
             </td>
         </tr>
     ${footer()}`, args,
