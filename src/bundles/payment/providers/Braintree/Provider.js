c7c35d55c64da29ee211318cbcd24eed899705d5
TolrenFrank <tolrenfrank1014@gmail.com>
2021-04-23 05:18:53 +0800

    cart and shipping address

--- src/bundles/payment/providers/Braintree/Provider.js
+++ src/bundles/payment/providers/Braintree/Provider.js
@@ -27,10 +27,10 @@ const TRANSACTION_SUCCESS_STATUSES = [
 class Provider extends ProviderAbstract {
   constructor(config, repository) {
     super();
-    this.client = new BraintreeGateway({
+    /*this.client = new BraintreeGateway({
       ...config,
       environment: Environment[config.environment[0].toUpperCase() + config.environment.slice(1)],
-    });
+    });*/
     this.repository = repository;
   }
 
@@ -60,8 +60,8 @@ class Provider extends ProviderAbstract {
       paymentMethodNonce,
       options: { submitForSettlement: true },
     })
-      .then(({ 
-        success, transaction, errors, message 
+      .then(({
+        success, transaction, errors, message
       }) => {
         if (success && TRANSACTION_SUCCESS_STATUSES.indexOf(transaction.status) !== -1) return transaction;
         return { error: message };
