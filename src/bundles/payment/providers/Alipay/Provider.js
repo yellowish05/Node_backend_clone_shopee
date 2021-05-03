67670b8bafa2f41f4b8d76384a2d7f05b2a6b24f
TolrenFrank <tolrenfrank1014@gmail.com>
2021-05-03 22:52:25 +0800

    alipay part 5

--- src/bundles/payment/providers/Alipay/Provider.js
+++ src/bundles/payment/providers/Alipay/Provider.js
@@ -80,7 +80,7 @@ class Provider extends ProviderAbstract {
     redirection,
   }) {
     // amount is in cents
-    console.log('buildSignOrderParam===>', this.client);
+    // console.log('buildSignOrderParam===>', this.client);
     const amountOfMoney = CurrencyFactory.getAmountOfMoney({ centsAmount: transaction.amount, currency: transaction.currency });
     console.log('amountOfMoney', amountOfMoney)
     if (!this.client) console.log('Alipay Connection Error !');
@@ -123,7 +123,7 @@ class Provider extends ProviderAbstract {
     formData.addField('bizContent', bizContent);
     /** 注：支付结果以异步通知为准，不能以同步返回为准，因为如果实际支付成功，但因为外力因素，如断网、断电等导致页面没有跳转，则无法接收到同步通知；* */
     /** 支付完成的跳转地址,用于用户视觉感知支付已成功，传值外网可以访问的地址，如果同步未跳转可参考该文档进行确认：https://opensupport.alipay.com/support/helpcenter/193/201602474937 * */
-    formData.addField('returnUrl', 'https://xiufu88.com/');
+    formData.addField('returnUrl', redirection.success);
     /** 异步通知地址，以http或者https开头的，商户外网可以post访问的异步地址，用于接收支付宝返回的支付结果，如果未收到该通知可参考该文档进行确认：https://opensupport.alipay.com/support/helpcenter/193/201602475759 * */
     formData.addField('notifyUrl', 'https://api.xiufu88.com/webhooks/payment/alipay');
     /** 第三方调用（服务商模式），传值app_auth_token后，会收款至授权token对应商家账号，如何获传值app_auth_token请参考文档：https://opensupport.alipay.com/support/helpcenter/79/201602494631 * */
