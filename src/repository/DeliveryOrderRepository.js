cac429d339490f559963d8d7a876319334b3e9b2
TolrenFrank <tolrenfrank1014@gmail.com>
2021-07-09 15:17:19 +0800

    purchase order

--- src/repository/DeliveryOrderRepository.js
+++ src/repository/DeliveryOrderRepository.js
@@ -10,15 +10,10 @@ class DeliveryOrderRepository {
   }
 
   async getByIds(ids) {
-    console.log({ids})
+    console.log({ ids });
     const deliveryOrders = await this.model.find({ _id: { $in: ids } });
     console.log('deliveryOrders=====>', deliveryOrders);
-    const tempOrders = [];
-    deliveryOrders.forEach((item) => {
-      tempOrders.push({ ...item, id: item._id });
-    });
-    console.log('tempOrders=====>', tempOrders);
-    return tempOrders;
+    return deliveryOrders;
   }
 
   async getAll() {
