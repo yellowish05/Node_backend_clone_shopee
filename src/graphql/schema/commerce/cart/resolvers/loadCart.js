d23a5cf13c91b0e62e9ab461d8310bf6b4a3c463
SoftwareDev1014 <tolrenfrank1014@gmail.com>
2021-09-11 16:39:49 -0400

    Update loadCart.js

--- src/graphql/schema/commerce/cart/resolvers/loadCart.js
+++ src/graphql/schema/commerce/cart/resolvers/loadCart.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-param-reassign */
 const { ForbiddenError } = require('apollo-server');
+const { DiscountValueType, DiscountPrivileges } = require(path.resolve('src/lib/Enums'));
 
 module.exports = async (_, args, { dataSources: { repository }, user }) => repository.userCartItem
   .getAll({ user: user.id })
